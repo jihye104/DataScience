8d58e420dcb26ef9858c37ecbcc29521cf13ea9c
TJ <rrlawlgp@naver.com>
2022-11-23 11:20:50 +0900

    update

---
+++ PythonBasic/Hello word.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+print("jupyter 테스트1")
+
+
+# In[20]:
+
+
+# A키는 현재 셀의 위에서 새로운 셀을 만든다.
+print("프로그램 실행 준비")
+
+num1 = int(input("첫 번째 수를 입력하세요"))
+# B키는 현재 셀의 아래에서 새로운 셀을 만든다.
+
+
+# In[21]:
+
+
+num1
+
+
+# In[22]:
+
+
+type(num1)
+
+
+# In[23]:
+
+
+num2 = int(input("두 번째 수를 입력하세요"))
+
+
+# In[24]:
+
+
+num2
+
+
+# In[25]:
+
+
+num1+num2
+
+
+# In[18]:
+
+
+print("jupyter 테스트1")
+
+# A키는 현재 셀의 위에서 새로운 셀을 만든다.
+print("프로그램 실행 준비")
+
+num1 = int(input("첫 번째 수를 입력하세요"))
+# B키는 현재 셀의 아래에서 새로운 셀을 만든다.
+
+num2 = int(input("두 번째 수를 입력하세요"))
+
+num1+num2
+
